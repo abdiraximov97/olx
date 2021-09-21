aa909da5bb4fff59872fb551df771a16ec835210
abdiraximov97 <80697768+abdiraximov97@users.noreply.github.com>
2021-09-21 14:26:24 +0500

    connect sass

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const path = require("path");
 const databaseMiddleware = require("./middlewares/databaseMiddleware");
 const routes = require("./routes/routes");
+const mongo = require("./modules/mongoose");
 
 
 const port = process.env.port || 8888;
@@ -21,10 +22,11 @@ async function server(mode) {
         app.use(cookieParser());
         app.use(express.json());
         app.use(express.urlencoded({ extended: true }));
-        app.use(express.static(path.join(__dirname, "src", "public")));
+
+        app.use(express.static(path.join(__dirname, "public")));
         app.use(databaseMiddleware);
 
-        app.use("/assets", express.static("assets"));
+        await mongo();
 
         if(mode == "DEV") {
             app.use(morgan("dev"));

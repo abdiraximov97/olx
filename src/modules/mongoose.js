f05b4a7db040b8425c3fe9501be881fa23ba3a4b
abdiraximov97 <80697768+abdiraximov97@users.noreply.github.com>
2021-09-22 14:09:57 +0500

    add usermodel

--- src/modules/mongoose.js
+++ src/modules/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const users = require("../models/userModel");
 
 async function mongo() {
     try {
         await mongoose.connect(process.env.MONGO_URL);
+
+
     } catch (error) {
         console.error("MONGOERROR: ", error);
     }

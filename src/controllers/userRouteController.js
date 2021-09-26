a08d21e96b9829c04d6cd9694d841f599cf807c2
abdiraximov97 <80697768+abdiraximov97@users.noreply.github.com>
2021-09-26 11:58:23 +0500

    email

--- src/controllers/userRouteController.js
+++ src/controllers/userRouteController.js
@@ -2,7 +2,7 @@ const users = require("../models/userModel");
 const { generateHash } = require("../modules/bcrypt");
 const { SignUpValidation } = require("../modules/validation");
 const {email: sendEmail} = require("../modules/email");
-const mailer = require("../modules/email");
+const path = require("path");
 
 module.exports = class UserRouteController {
     static async UserRegistrationGetController(req, res) {
@@ -12,54 +12,35 @@ module.exports = class UserRouteController {
         res.render("login");
     };
     static async UserSignUpPostController(req, res) {
-        // try {
-        //     const {name, email, password} = await SignUpValidation(req.body);
-
-        //     const user  =  await users.create({
-        //         name, 
-        //         email, 
-        //         password: await generateHash(password), 
-        //     });
-
-
-        //     await sendEmail(
-        //             email, 
-        //             "Iltimos pochtangizni tasdiqlang!", 
-        //             `Pochtangizni tasdiqlash uchun link`,
-        //             `<a href="http://localhost:8888/users/verify/${user._id}">Tasdiqlash</a>`
-        //         );
-
-        //     res.redirect("/login");
-
-        //     console.log(user);
-
-        // } catch (error) {
-        //     console.log(error);
-        //     res.render("signup", {
-        //         error: error + "",
-        //     });
-        // }
-
         try {
-            if(!req.body.email || !req.body.password) return res.sendStatus(400);
-            const message = {
-                to: req.body.email,
-                subject: "Tabriklaymiz! Siz saytingizda muvaffaqiyatli ro'yxatdan o'tdingiz",
-                text: `Tabriklaymiz! Siz saytingizda muvaffaqiyatli ro'yxatdan o'tdingiz
-                
-                sizning shaxsiy ma'lumotlaringiz:
-                login: ${req.body.email}
-                password: ${req.body.password}
-                
-                shaxsiy malumotlaringizni hech kimga bermang`
-            };
-            mailer(message);
-            const user = req.body;
-            res.redirect("login");
+            const {name, email, password} = await SignUpValidation(req.body);
+
+            const user  =  await users.create({
+                name, 
+                email, 
+                password: await generateHash(password), 
+            });
+
+
+            await sendEmail(
+                    email, 
+                    "Iltimos pochtangizni tasdiqlang!", 
+                    `Pochtangizni tasdiqlash uchun link`,
+                    `<a href="http://localhost:8888/users/verify/${user._id}">Tasdiqlash</a>`
+                );
+
+            res.redirect("/login");
+
+            console.log(user);
+
         } catch (error) {
-            console.error(error);
+            console.log(error);
+            res.render("signup", {
+                error: error + "",
+            });
         }
 
+        
     };
 
     static async UserVerifyGetController(req, res) {
